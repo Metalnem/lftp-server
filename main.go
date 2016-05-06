98f70456d1982e547f9148712109b5b6ca1c4de1
Metalnem <metalnem@mijailovic.net>
2016-05-06 20:02:25 +0200

    Mirroring single folder for one hard-coded job

--- main.go
+++ main.go
@@ -1,7 +1,65 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+	"log"
+	"net/url"
+	"os"
+	"os/exec"
+)
+
+// Request represents single request for mirroring one FTP directory or a file.
+type Request struct {
+	URL      string
+	Username string
+	Password string
+}
+
+func (request *Request) makeCmd() (*exec.Cmd, error) {
+	if request.URL == "" {
+		return nil, errors.New("No URL specified in a request")
+	}
+
+	url, err := url.Parse(request.URL)
+
+	if err != nil {
+		return nil, fmt.Errorf("Invalid URL: %s", request.URL)
+	}
+
+	cmd := exec.Command(
+		"lftp",
+		"-u", fmt.Sprintf("%s,%s", request.Username, request.Password),
+		"-e", fmt.Sprintf("mirror %s && exit", url.Path),
+		fmt.Sprintf("%s://%s", url.Scheme, url.Host),
+	)
+
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+
+	return cmd, nil
+}
 
 func main() {
-	fmt.Println("Hello, LFTP server!")
+	if _, err := exec.LookPath("lftp"); err != nil {
+		log.Fatal("LFTP not found")
+	}
+
+	request := Request{
+		URL:      "ftp://example.org/",
+		Username: "username",
+		Password: "password",
+	}
+
+	cmd, err := request.makeCmd()
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = cmd.Run()
+
+	if err != nil {
+		log.Fatal(err)
+	}
 }
