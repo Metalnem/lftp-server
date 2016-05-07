610cadc8c9878a52ebb5b5b2df6ae2bc849a15a5
Metalnem <metalnem@mijailovic.net>
2016-05-07 19:03:49 +0200

    Server mostly working

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bytes"
 	"encoding/json"
 	"errors"
 	"fmt"
@@ -9,6 +10,7 @@ import (
 	"net/url"
 	"os"
 	"os/exec"
+	"time"
 )
 
 // Request represents single request for mirroring one FTP directory or a file.
@@ -16,6 +18,11 @@ type Request struct {
 	Path     string `json:"path"`
 	Username string `json:"username"`
 	Password string `json:"password"`
+}
+
+// Handler implements http.Handler interface and logs errors to custom log.Logger.
+type Handler struct {
+	Logger *log.Logger
 }
 
 func (request *Request) makeCmd() (*exec.Cmd, error) {
@@ -32,7 +39,7 @@ func (request *Request) makeCmd() (*exec.Cmd, error) {
 	cmd := exec.Command(
 		"lftp",
 		"-u", fmt.Sprintf("%s,%s", request.Username, request.Password),
-		"-e", fmt.Sprintf("mirror %s && exit", url.Path),
+		"-e", fmt.Sprintf("mirror '%s' && exit", url.Path),
 		fmt.Sprintf("%s://%s", url.Scheme, url.Host),
 	)
 
@@ -42,27 +49,26 @@ func (request *Request) makeCmd() (*exec.Cmd, error) {
 	return cmd, nil
 }
 
-func handler(w http.ResponseWriter, r *http.Request) {
+func (handler *Handler) handle(w http.ResponseWriter, r *http.Request) error {
 	var request Request
 	decoder := json.NewDecoder(r.Body)
 
 	if err := decoder.Decode(&request); err != nil {
-		fmt.Fprintln(os.Stderr, "Invalid request received")
-		return
+		return fmt.Errorf("Invalid request received: %v", err)
 	}
 
 	cmd, err := request.makeCmd()
 
 	if err != nil {
-		fmt.Fprintln(os.Stderr, err.Error())
-		return
+		return err
 	}
 
-	err = cmd.Run()
+	return cmd.Run()
+}
 
-	if err != nil {
-		fmt.Fprintln(os.Stderr, err.Error())
-		return
+func (handler *Handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	if err := handler.handle(w, r); err != nil {
+		handler.Logger.Println(err)
 	}
 }
 
@@ -71,6 +77,36 @@ func main() {
 		log.Fatal("LFTP not found")
 	}
 
-	handler := http.HandlerFunc(handler)
-	log.Fatal(http.ListenAndServe(":7800", handler))
+	request := Request{
+		Path:     "ftp://example.org/path",
+		Username: "user",
+		Password: "pass",
+	}
+
+	logger := log.New(os.Stderr, "ERROR: ", log.Ldate|log.Ltime|log.Lshortfile)
+	buffer := new(bytes.Buffer)
+	encoder := json.NewEncoder(buffer)
+
+	if err := encoder.Encode(request); err != nil {
+		log.Fatal(err)
+	}
+
+	go func() {
+		time.Sleep(time.Second)
+		resp, err := http.Post("http://localhost:7800/jsonrpc", "application/json", buffer)
+
+		if err != nil {
+			logger.Println(err)
+			return
+		}
+
+		defer resp.Body.Close()
+
+		if resp.StatusCode != http.StatusOK {
+			logger.Println(resp.Status)
+		}
+	}()
+
+	http.Handle("/jsonrpc", &Handler{Logger: logger})
+	log.Fatal(http.ListenAndServe(":7800", nil))
 }
